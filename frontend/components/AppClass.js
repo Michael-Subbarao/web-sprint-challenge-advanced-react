0e66b9682a5da707d3771dd0b3d97c72e26b1851
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-03-11 17:09:51 -0500

    AppClass fits MVP

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -7,7 +7,7 @@ const initialState = {
   y: 2,
   email: '',
   steps: 0,
-  grid: [[null, null, null], [null, "B", null], [null, null, null]]
+  message: '',
 }
 
 export default class AppClass extends React.Component {
@@ -16,59 +16,129 @@ export default class AppClass extends React.Component {
     this.state = initialState;
   }
   //When Resetting
-  reset = () =>{
-    this.setState(initialState);
-  }
-  onChange = (key, value) => {
-    this.setState({
-      ...this.state,
-      form: { ...this.state.form, [key]: value },
-    })
-  }
-  onSubmit = e =>{
-    e.preventDefault()
-    axios.post(URL, this.state)
-      .then(reponse =>{
-        this.setState({
-          
-        })
-      })
-    
-  }
+  
   render() {
     const { className } = this.props
+    const {x, y, steps, email, message} = this.state
+
+    const reset = () =>{
+      this.setState(initialState);
+      this.setState({
+        message: ''
+      })
+    }
+
+    const onChange = e => {
+      const {value} = e.target
+      this.setState({...this.state, email: value})
+    }
+    
+
+    //movement
+    const left = () =>{
+      if(x>1){
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          x: this.state.x - 1,
+        })
+      }
+      else {
+        this.setState({message: "You can't go left"})
+      }
+    }
+
+    const right = () =>{
+      if(x<3){
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          x: this.state.x + 1,
+        })
+      }
+      else {
+        this.setState({message: "You can't go right"})
+      }
+    }
+
+    const up = () =>{
+      if(y>1){
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          y: this.state.y - 1,
+        })
+      }
+      else {
+        this.setState({message: "You can't go up"})
+      }
+    }
+    const down = () =>{
+      if(y<3){
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          y: this.state.y + 1,
+        })
+      }
+      else {
+        this.setState({message: "You can't go down"})
+      }
+    }
+
+    const onSubmit = event => {
+      event.preventDefault()
+     
+      axios.post('http://localhost:9000/api/result', {
+        'x': x,
+        'y': y,
+        'steps': steps,
+        'email': email
+      })
+      .then(response=> {
+        this.setState({...this.state, message: response.data.message})
+        this.setState({...this.state, email: ''})
+      })
+      .catch(error=> {
+        this.setState({...this.state, message: error.response.data.message})
+      })
+      this.setState({
+        email: ''
+      })
+    }
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({x}, {y})</h3>
+          <h3 id="steps">You moved {steps} {steps===1?'time': 'times'}</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div className= { this.state.x ===1 && this.state.y === 1 ? 'square active' : 'square '}>{this.state.x ===1 && this.state.y === 1 ? 'B' : ''}</div>
+          <div className={ this.state.x === 2 && this.state.y === 1 ? 'square active' : 'square '}>{this.state.x ===2 && this.state.y === 1 ? 'B' : ''}</div>
+          <div className={ this.state.x === 3 && this.state.y === 1 ? 'square active' : 'square '}>{this.state.x ===3 && this.state.y === 1 ? 'B' : ''}</div>
+          <div className={ this.state.x === 1 && this.state.y === 2 ? 'square active' : 'square '}>{this.state.x ===1 && this.state.y === 2 ? 'B' : ''}</div>
+          <div className={ this.state.x === 2 && this.state.y === 2 ? 'square active' : 'square '}>{this.state.x ===2 && this.state.y === 2 ? 'B' : ''}</div>
+          <div className={ this.state.x === 3 && this.state.y === 2 ? 'square active' : 'square '}>{this.state.x ===3 && this.state.y === 2 ? 'B' : ''}</div>
+          <div className={ this.state.x === 1 && this.state.y === 3 ? 'square active' : 'square '}>{this.state.x ===1 && this.state.y === 3 ? 'B' : ''}</div>
+          <div className={ this.state.x === 2 && this.state.y === 3 ? 'square active' : 'square '}>{this.state.x ===2 && this.state.y === 3 ? 'B' : ''}</div>
+          <div className={ this.state.x === 3 && this.state.y === 3 ? 'square active' : 'square '}>{this.state.x ===3 && this.state.y === 3 ? 'B' : ''}</div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick = {left}>LEFT</button>
+          <button id="up" onClick = {up}>UP</button>
+          <button id="right" onClick = {right}>RIGHT</button>
+          <button id="down" onClick = {down}>DOWN</button>
+          <button id="reset" onClick = {reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" onChange={(this.onChange)}></input>
-          <input id="submit" type="submit" onSubmit = {this.onSubmit}></input>
+        <form onSubmit = {onSubmit}>
+        <input id="email" type="email" placeholder="type email" onChange={onChange} value={this.state.email}></input>
+          <input id="submit" type="submit"></input>
         </form>
       </div>
     )
   }
 }
+//value={this.state.email}
