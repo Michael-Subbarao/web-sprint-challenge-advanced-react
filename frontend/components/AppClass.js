a0bf942051d7990a65f45fdb818f2ad268c03c7d
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-03-11 15:47:37 -0500

    set up state and added constructor and some properties to class based component

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,6 +1,40 @@
 import React from 'react'
+import axios from 'axios'; 
+const URL = 'http://localhost:9000/api/result';
+
+const initialState = {
+  x: 2,
+  y: 2,
+  email: '',
+  steps: 0,
+  grid: [[null, null, null], [null, "B", null], [null, null, null]]
+}
 
 export default class AppClass extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = initialState;
+  }
+  //When Resetting
+  reset = () =>{
+    this.setState(initialState);
+  }
+  onChange = (key, value) => {
+    this.setState({
+      ...this.state,
+      form: { ...this.state.form, [key]: value },
+    })
+  }
+  onSubmit = e =>{
+    e.preventDefault()
+    axios.post(URL, this.state)
+      .then(reponse =>{
+        this.setState({
+          
+        })
+      })
+    
+  }
   render() {
     const { className } = this.props
     return (
@@ -31,8 +65,8 @@ export default class AppClass extends React.Component {
           <button id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input id="email" type="email" onChange={(this.onChange)}></input>
+          <input id="submit" type="submit" onSubmit = {this.onSubmit}></input>
         </form>
       </div>
     )
